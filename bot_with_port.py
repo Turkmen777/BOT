62e40c85721f95159934acbfb984501ca9e7fe42
Turkmen777 <artyomgurbangulyyew1998@gmail.com>
2026-03-12 21:25:26 +0300

    Update bot_with_port.py

--- bot_with_port.py
+++ bot_with_port.py
@@ -21,7 +21,7 @@ def run_flask():
     app.run(host='0.0.0.0', port=10000, debug=False, use_reloader=False)
 
 # ========== НАСТРОЙКИ БОТА ==========
-BOT_TOKEN = "8741918027:AAEqpPPZBDO54UZcmxyJb_U4gfuVqc97j5w"
+BOT_TOKEN = "8741918027:AAEqpPPZBDO54UZcmxyJb_U4gfuVqc97j5"
 GROUP_CHAT_ID = -1003759188641
 ADMIN_IDS = [8444800411]
 MIN_AMOUNT = 50
@@ -222,3 +222,4 @@ def main():
 if __name__ == '__main__':
 
     main()
+
